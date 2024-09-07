cffdc792e1459ace4f24881f91923c631d289a1b
Yashwanth Sai Sollu <www.sai1929@gmail.com>
2024-09-07 19:36:41 +0530

    To Test on some Random Acc

--- trans_contracts/src/contract_deploy.rs
+++ trans_contracts/src/contract_deploy.rs
@@ -14,10 +14,27 @@ async fn main() -> Result<()>{
     let ganache = Ganache::new().menomic(mnemonic).spawn();
     println!("HTTP EndPoint: {}",ganache.endpoint());
 
+    //Creating a local wallet
     let wallet: Localwallet = ganche.keys()[0].clone.into();
     let first_address = wallet.address();
     println!(
         "Wallet First Address: {}",first_address.encode_hex::<String>()
+    );
+
+    //Provider
+    let provider = Provider::try_from(ganache.endpoint())?.interval(Duration::from_millis(10));
+
+    //Query the balance 
+    let first_balance = provider.get_balance(first_address,None).await?;
+    println!("Wallet First Address Balance: {}", first_balance);
+
+    //Random Acc
+    let other_address_hex = "0xaf206dCE72A0ef76643dfeDa34DB764E2126E646";
+    let other_address = "0xaf206dCE72A0ef76643dfeDa34DB764E2126E646".parse::<Address>()?;
+
+    let other_balance = provider.get_balance(other_address,None).await?;
+    println!(
+        "Balance For the Address {}: {}",other_address_hex,other_balance
     );
 
     
